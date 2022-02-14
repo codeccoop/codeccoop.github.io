51755c0d8c986ac46661159a11cb7da93dd04610
Pau <pau.pulidocompanys@gmail.com>
2022-02-14 19:28:46 +0100

    canvis disseny

--- assets/js/utils/background-patterns.js
+++ assets/js/utils/background-patterns.js
@@ -128,29 +128,33 @@ var BackgroundPatterns = (function () {
         return {
           home:
             '<svg width="{100vw}" height="{100vh}" xmlns="http://www.w3.org/2000/svg">' +
+            //(window.innerWidth > 700
+            //  ? '<path d="M {2vw}, 210 V {100vh} H {9vw} V 90 Z" fill="#525393ff" />'
+            //  : '<path d="M 0, 210 V {100vh} H {9vw} V 90 Z" fill="#525393ff" />') +
+            '<path d="M 0, {80vh} H {2vw} L {2vw}, {95vh} H 0 Z" fill="#041734ff"/>'+
+            '<path d="M {9vw},{95vh} V {100vh} H {2vw} V {80vh} Z" fill="#fe9a00ff"/>'+
             (window.innerWidth > 700
-              ? '<path d="M {2vw}, 300 V {100vh} H {9vw} V 90 Z" fill="#40a1df" />'
-              : '<path d="M 0, 300 V {100vh} H {9vw} V 90 Z" fill="#40a1df" />') +
-            (window.innerWidth > 700
-              ? '<path d="M {2vw}, 300 H {82vw} V 90 H {9vw} Z" fill="#8edf00" />'
-              : '<path d="M 0, 200 V 300 H {82vw} V 90 H {9vw} Z" fill="#8edf00" />') +
-            '<path d="M {82vw}, 300 V 0 H {92vw} V 90 Z" fill="#df0094"/>' +
+              ? '<path d="M 0, calc(110px + {15vh}) H {82vw} V 110 H 0 Z" fill="#041734ff" />'
+              : '<path d="M 0, 210 V 200 H {82vw} V 90 H {9vw} Z" fill="#041734ff" />') +
+            '<path d="M {82vw}, calc(110px + {15vh}) V 0 H {90vw} V 110 Z" fill="#fe9a00ff"/>' +
             "</svg>",
           work:
             '<svg width="{100vw}" height="{100vh}" xmlns="http://www.w3.org/2000/svg">' +
             (window.innerWidth > 700
-              ? '<path d="M {2vw}, 0 V {45vh} H max(50px, {9vw}) V 0 Z" fill="#40a1df" />'
-              : '<path d="M 0, 0 V {45vh} H {9vw} V 0 Z" fill="#40a1df" />') +
-            '<path d="M {98vw}, {100vh} V {55vh} H min({91vw}, calc({98vw} - 50px)) V {100vh} Z" fill="#8edf00" />' +
+              ? '<path d="M {2vw}, 0 V {45vh} H max(50px, {9vw}) V 0 Z" fill="#fe9a00ff" />'
+              : '<path d="M 0, 0 V {45vh} H {9vw} V 0 Z" fill="#fe9a00ff" />') +
+            '<path d="M {98vw}, {100vh} V {55vh} H min({91vw}, calc({98vw} - 50px)) V {100vh} Z" fill="#041734ff" />' +
             "</svg>",
           team:
             '<svg width="{100vw}" height="{100vh}" xmlns="http://www.w3.org/2000/svg">' +
-            '<path d="M {98vw}, {90vh} V 0 H min({91vw}, calc({98vw} - 50px)) V {98vh} Z" fill="#8edf00" />' +
-            '<path d="M {98vw}, {90vh} H max({9vw}, 50px) V {98vh} H {92vw} Z" fill="#40a1df" />' +
-            '<path d="M max(50px, {9vw}), {90vh} V {100vh} H {2vw} V {98vh} Z" fill="#df0094"/>' +
+            '<path d="M {98vw}, {90vh} V 0 H min({91vw}, calc({98vw} - 50px)) V {98vh} Z" fill="#041734ff" />' +
+            '<path d="M {98vw}, {90vh} H max({9vw}, 50px) V {98vh} H {92vw} Z" fill="#fe9a00ff" />' +
+            '<path d="M max(50px, {9vw}), {90vh} V {100vh} H {2vw} V {98vh} Z" fill="#041734ff"/>' +
             "</svg>",
           default:
             '<svg width="{100vw}" height="{100vh}" xmlns="http://www.w3.org/2000/svg">' +
+            '<path d="M 0, 90 H {5vw} V calc(90px + {8vw}) H 0 Z" fill="#f6ff62" />' +
+            '<path d="M {5vw}, 90 H {90vw} V calc(90px + {8vw}) H {5vw} Z" fill="#005f89" />' + '<path d="M 0, calc(90px + {8vw}) H {5vw} V {50vh} H 0 Z" fill="#f76464" />'+
             "</svg>",
         };
       },
