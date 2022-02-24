a0e5373fc67aa6410a9137d7de003f68e65cbcfa
Bruno Carrión <brunocarrionpinar@protonmail.com>
2022-02-24 12:12:34 +0100

    Expand product descriptions;

--- assets/js/pages/products.js
+++ assets/js/pages/products.js
@@ -7,4 +7,32 @@ document.addEventListener("DOMContentLoaded", function() {
         });
         background.bind(rootEl);
     }, 0);
+
+	var description
+	var descriptions = document.getElementsByClassName("products__product-toggle");
+	for (var i=0; i<descriptions.length; i++) {
+		function toggleVisibility (ev) {
+			if (!ev.target.classList.contains("products__product-description")) {
+				ev.currentTarget.classList.toggle("open");
+				if (ev.currentTarget.classList.contains("open")) {
+					ev.target.setAttribute("aria-pressed", "true");
+					ev.currentTarget.setAttribute("aria-expanded", "true");
+				} else {
+					ev.target.setAttribute("aria-pressed", "false");
+					ev.currentTarget.setAttribute("aria-expanded", "false");
+				}
+			}
+		}
+
+		function bindToEnter (fn, keyCode) {
+			return function (ev) {
+				if (ev.keyCode === keyCode) {
+					fn(ev);
+				}
+			}
+		}
+
+		descriptions[i].addEventListener("click", toggleVisibility);
+		descriptions[i].addEventListener("keydown", bindToEnter(toggleVisibility, 13));
+	}
 });
