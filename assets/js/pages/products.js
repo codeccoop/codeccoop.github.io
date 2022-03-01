163a1ef5ab5dcc1929cafa4835129af6f311a1db
Bruno Carrión <brunocarrionpinar@protonmail.com>
2022-03-01 16:32:04 +0100

    Re-render background on products visibility changes; Use svg backgroundImage on re-render backgrounds as a fallback background until png isn't ready;

--- assets/js/pages/products.js
+++ assets/js/pages/products.js
@@ -1,38 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
     var rootEl = document.getElementsByTagName("main")[0];
 
-    setTimeout(function() {
-        var background = new BackgroundPatterns("products", {
-            isMobile: isMobile()
-        });
-        background.bind(rootEl);
-    }, 0);
+    var renderBackground = (function(el) {
+        return function() {
+            var background = new BackgroundPatterns("products", {
+                isMobile: isMobile()
+            });
+            background.bind(rootEl);
+        }
+    })(rootEl);
+    setTimeout(renderBackground, 0);
 
-	var description
-	var descriptions = document.getElementsByClassName("products__product-toggle");
-	for (var i=0; i<descriptions.length; i++) {
-		function toggleVisibility (ev) {
-			if (!ev.target.classList.contains("products__product-description")) {
-				ev.currentTarget.classList.toggle("open");
-				if (ev.currentTarget.classList.contains("open")) {
-					ev.currentTarget.setAttribute("aria-pressed", "true");
-					ev.currentTarget.setAttribute("aria-expanded", "true");
-				} else {
-					ev.currentTarget.setAttribute("aria-pressed", "false");
-					ev.currentTarget.setAttribute("aria-expanded", "false");
-				}
-			}
-		}
+    var description
+    var descriptions = document.getElementsByClassName("products__product-toggle");
+    for (var i = 0; i < descriptions.length; i++) {
+        function toggleVisibility(ev) {
+            if (!ev.target.classList.contains("products__product-description")) {
+                ev.currentTarget.classList.toggle("open");
+                if (ev.currentTarget.classList.contains("open")) {
+                    ev.currentTarget.setAttribute("aria-pressed", "true");
+                    ev.currentTarget.setAttribute("aria-expanded", "true");
+                } else {
+                    ev.currentTarget.setAttribute("aria-pressed", "false");
+                    ev.currentTarget.setAttribute("aria-expanded", "false");
+                }
+				setTimeout(renderBackground(), 0);
+            }
+        }
 
-		function bindToEnter (fn, keyCode) {
-			return function (ev) {
-				if (ev.keyCode === keyCode) {
-					fn(ev);
-				}
-			}
-		}
+        function bindToEnter(fn, keyCode) {
+            return function(ev) {
+                if (ev.keyCode === keyCode) {
+                    fn(ev);
+                }
+            }
+        }
 
-		descriptions[i].addEventListener("click", toggleVisibility);
-		descriptions[i].addEventListener("keydown", bindToEnter(toggleVisibility, 13));
-	}
+        descriptions[i].addEventListener("click", toggleVisibility);
+        descriptions[i].addEventListener("keydown", bindToEnter(toggleVisibility, 13));
+    }
 });
