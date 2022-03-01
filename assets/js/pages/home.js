3681a7ce1cc1070836deb681f9c26c47bdeb014e
Pau <pau.pulidocompanys@gmail.com>
2022-03-01 16:23:48 +0100

    desplegables-home

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -23,4 +23,39 @@ document.addEventListener("DOMContentLoaded", function() {
         });
         teamBg.bind(document.getElementById("team"));
     }, 0);
+
+    var description
+	var descriptions = document.getElementsByClassName("us__information-toggle");
+	for (var i=0; i<descriptions.length; i++) {
+		function toggleVisibility (ev) {
+			if (!ev.target.classList.contains("us__information-description")) {
+				ev.currentTarget.classList.toggle("open");
+				if (ev.currentTarget.classList.contains("open")) {
+					ev.target.setAttribute("aria-pressed", "true");
+					ev.currentTarget.setAttribute("aria-expanded", "true");
+				} else {
+					ev.target.setAttribute("aria-pressed", "false");
+					ev.currentTarget.setAttribute("aria-expanded", "false");
+				}
+                var teamBg = new BackgroundPatterns("team", {
+                    isMobile: isMobile()
+                });
+                teamBg.bind(document.getElementById("team"))
+			}
+		}
+
+		function bindToEnter (fn, keyCode) {
+			return function (ev) {
+				if (ev.keyCode === keyCode) {
+					fn(ev);
+				}
+			}
+		}
+
+		descriptions[i].addEventListener("click", toggleVisibility);
+		descriptions[i].addEventListener("keydown", bindToEnter(toggleVisibility, 13));
+	}
+
+
+
 });
